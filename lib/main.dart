0f37ff11ca33c2383de2b7bd691a3ed138a854f2
vinothvino42 <vinothvino42@gmail.com>
2023-10-18 19:50:50 +0530

    Add external methods and update hello home view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 import 'dart:developer';
+import 'package:ensemble/action/call_external_method.dart';
 import 'package:ensemble/ensemble_app.dart';
 import 'package:ensemble/framework/error_handling.dart';
 import 'package:ensemble/framework/widget/error_screen.dart';
@@ -8,14 +9,24 @@ import 'package:ensemble/page_model.dart';
 import 'package:flutter/material.dart';
 import 'package:starter_module/generated/ensemble_modules.dart';
 
-
 /// this demonstrates an App running exclusively with Ensemble
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   initErrorHandler();
   EnsembleModules().init();
   HostPlatformManager().init();
-  runApp(const EnsembleApp());
+  runApp(
+    const EnsembleApp(
+      externalMethods: {
+        'sendDataToSwiftUI': sendDataToSwiftUI,
+      },
+    ),
+  );
+}
+
+void sendDataToSwiftUI() {
+  print('sendDataToSwiftUI called');
+  HostPlatformManager().sendData({'title': 'Hello everyone'});
 }
 
 void initErrorHandler() {
